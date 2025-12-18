4c5cd0115277b3048d2564df081a2d6ec943d78b
JuanSeFC <juan.canarte@correounivalle.edu.co>
2025-12-17 19:18:42 -0500

    Agregue un administrador de datos inicial para el manejo de puntos/figuras y una interfaz de usuario Tkinter del menú principal para interacciones de puntos de pruba

--- core/data/data_manager.py
+++ core/data/data_manager.py
@@ -2,21 +2,25 @@ import json
 from core.domain.point import Point
 from core.domain.figure import Figure
 
+
 class DataManager:
     """
     Handles saving and loading points and figures from files.
     """
-    
-    #Listas de pruebas
+
+    # Listas de pruebas
     def test_list(self, number):
         lists = [[Point(0, 0), Point(3, 0), Point(0, 4), Point(3, 4)],
-                 [Point(3, 20), Point(14, 9), Point(4, 5), Point(3, 2),Point(15, 2), Point(-4, -5), Point(7, -5), Point(24, -5),Point(7, -24)],
-                 [Point(1,1), Point(1,5), Point(5,1), Point(1,-2), Point(5,-2)],
-                 []
+                 [Point(3, 20), Point(14, 9), Point(4, 5), Point(3, 2), Point(
+                     15, 2), Point(-4, -5), Point(7, -5), Point(24, -5), Point(7, -24)],
+                 [Point(1, 1), Point(1, 5), Point(5, 1),
+                  Point(1, -2), Point(5, -2)],
+                 [Point(2, 1), Point(2, 5), Point(6, 5),
+                  Point(6, 1), Point(2, 5), Point(4, 8)]
                  ]
 
         return lists[number]
-    
+
     def __init__(self):
         self.points = []
         self.figures = []
@@ -28,7 +32,7 @@ class DataManager:
         return True
 
     def add_points(self, points):
-        
+
         pass
 
     def get_points(self):
@@ -42,4 +46,3 @@ class DataManager:
 
     def load_points(self):
         pass  # luego lo implementas
-
